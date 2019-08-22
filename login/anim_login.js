60c154130be3c96c15135509dc8275335731d0a4
Redz <m.lemoniesdesagazan@gmail.com>
2019-08-22 22:40:20 +0200

    login animation changed

--- login/anim_login.js
+++ login/anim_login.js
@@ -1,6 +1,5 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const rc = rough.canvas(document.getElementById('canvas'));
 let lastInterval = -1;
 
 canvas.style.position = "absolute";
@@ -11,43 +10,63 @@ canvas.style.left = "0px";
 canvas.style.top = "0px";
 
 ctx.fillStyle = "white";
-ctx.strokeStyle = "white";
-ctx.lineWidth = 10;
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 1;
 
-const spots = [
-    {x: 0, y: 0},
-    {x: 0, y: innerHeight},
-    {x: innerWidth, y: 0},
-    {x: innerWidth, y: innerHeight}
-];
+
+function getSign(sign, spot, target) {
+    const baseSpots = [
+        {x: 0, y: 0},
+        {x: 0, y: innerHeight},
+        {x: innerWidth, y: 0},
+        {x: innerWidth, y: innerHeight}
+    ];
+    sign.x = (baseSpots[spot].x < target.x) ? 1 : -1;
+    sign.y = (baseSpots[spot].y < target.y) ? 1 : -1;
+}
 
 document.body.onclick = e => {
     let grow = 1;
     if (lastInterval !== -1) {
         clearInterval(lastInterval);
     }
-    lastInterval = setInterval(() => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    let iter = 1;
+    const spots = [
+        {x: 0, y: 0},
+        {x: 0, y: innerHeight},
+        {x: innerWidth, y: 0},
+        {x: innerWidth, y: innerHeight}
+    ];
 
+    const add = {
+        x: e.clientX / 10,
+        y: e.clientY / 10
+    };
+
+    const target = {
+        x: e.clientX,
+        y: e.clientY
+    };
+
+    lastInterval = setInterval(() => {
+        
         for (let i = 0; i < spots.length; i++) {
-            const obj = spots[i];
-            const pos = {
-                x: grow,
-                y: (grow * (e.clientY - obj.y)) / (e.clientX - obj.x) 
+            let sign = {
+                x: 1,
+                y: 1
             };
+            getSign(sign, i, target);
             ctx.beginPath();
-            ctx.moveTo(obj.x, obj.y);
-            if (e.clientX < obj.x) {
-                ctx.lineTo(obj.x - pos.x, obj.y - pos.y);
+            ctx.moveTo(spots[i].x, spots[i].y);
+            if (iter % 2) {
+                spots[i].x += add.x * sign.x;
             } else {
-                ctx.lineTo(obj.x + pos.x, obj.y + pos.y);
+                spots[i].y += add.y * sign.y;
             }
+            ctx.lineTo(spots[i].x, spots[i].y);
             ctx.stroke();
         }
-        // rc.line(0, 0, pos.x, pos.y, {
-        //     stroke: 'white',
-        //     strokeWidth: 3
-        // });
-        grow += 10;
+        iter++;
     }, 30);
 }
